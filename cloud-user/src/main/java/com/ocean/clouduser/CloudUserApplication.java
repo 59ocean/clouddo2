473ef6b6081b2d2a4ad841acb5a40d03fd1f6dc5
chenhy <chenhy@caibab.com>
2019-01-28 16:12:14 +0800

    Initial commit

---
+++ cloud-user/src/main/java/com/ocean/clouduser/CloudUserApplication.java
@@ -0,0 +1,14 @@
+package com.ocean.clouduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudUserApplication.class, args);
+	}
+
+}
+
